f49ff107963b61b0552500fc98cc392178257595
Me <Mubbasher@macbook-pro.local>
2016-08-06 08:14:22 +0100

    Added comment

---
+++ demo2/demo2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  demo2
+//
+//  Created by Mubbasher Khanzada on 06/08/2016.
+//  Copyright © 2016 mubb. All rights reserved.
+//  This is a comment
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
