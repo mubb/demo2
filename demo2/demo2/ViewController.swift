93a25de3699abfa7fd9c7b6ebe8371fb9d46d3f0
Me <Mubbasher@macbook-pro.local>
2016-08-06 08:19:16 +0100

    Deleted override Function

--- demo2/demo2/ViewController.swift
+++ demo2/demo2/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Mubbasher Khanzada on 06/08/2016.
 //  Copyright © 2016 mubb. All rights reserved.
 //  This is a comment
-//
+//  This is comment 2
 
 import UIKit
 
@@ -16,11 +16,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
